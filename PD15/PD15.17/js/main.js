73493e4f04ac0e4a1c375777f1f5a0cfdfc7e482
Edgaras Valansevicius <Edgaras.Valansevicius@stud.vtmc.lt>
2023-12-13 10:21:45 +0200

    15 exercise push

---
+++ PD15/PD15.17/js/main.js
@@ -0,0 +1,8 @@
+let num = 0;
+num++; 
+console.log(num); 
+
+num++; 
+console.log(num);
+num++; 
+console.log(num);
---
+++ PD15/PD15.20/js/main.js
@@ -0,0 +1,2 @@
+let index = [1,3,4,'hello',NaN,3].findIndex(Number.isNaN)
+console.log(index)
